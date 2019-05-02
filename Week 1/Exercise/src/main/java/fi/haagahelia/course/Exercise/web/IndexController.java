04cfd5c87a7aede17fd4c3f6f807b0e8fea93a93
Giang Nguyen <thuygiang152@gmail.com>
2019-05-02 22:03:46 +0300

    Add files via upload

--- Week 1/Exercise/src/main/java/fi/haagahelia/course/Exercise/web/IndexController.java
+++ Week 1/Exercise/src/main/java/fi/haagahelia/course/Exercise/web/IndexController.java
@@ -3,6 +3,8 @@ package fi.haagahelia.course.Exercise.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -14,9 +16,13 @@ public class IndexController {
 		return "This is the main page";
 }
 
-@RequestMapping("/contact")
-	public String contact() {
-		return "This is the contact page";
-}
-
+	@RequestMapping("/contact")
+		public String contact() {
+			return "This is the contact page";
+	}
+	
+	@RequestMapping("/hello")
+		public String hello(@RequestParam(name="location") String locationValue, @RequestParam(name="name") String nameValue ) {
+			return "Welcome to the " + locationValue + " "+ nameValue + "!";
+	}
 }
